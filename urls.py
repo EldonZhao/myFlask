c8ecbe9ec305dbc671d22de1bc95b9903b550ad4
EldonZhao <xyz_213083464@126.com>
2019-12-29 23:43:07 +0800

    增加接口，测试用例，优化启动参数，支持flaskenv

--- urls.py
+++ urls.py
@@ -3,9 +3,14 @@
 # @date: 2019年12月26日00:51:03
 
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
-@app.route('/user', method=['GET'])
+@app.route('/user', methods=['GET'])
 def get_users(*args, **kwargs):
-    return "OK"
+    return json.dumps([{'name': 'eldon', 'gendor': 'male'}])
+
+@app.route('/user/<name>', methods=['GET'])
+def get_user_by_name(name):
+    return {'name': name, 'gendor': 'male'}
--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@
 # @author: xueyong.zxy
 # @date: 2019年12月19日00:46:27
 
-from urls import app
+from .urls import app
 
 if __name__ == "__main__":
     app.run()
